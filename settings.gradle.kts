656849f9cd18d4e297e9b2785a0b1209407f18eb
iury souza <iurysza@users.noreply.github.com>
2023-03-15 14:44:22 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
+rootProject.name = "kotlin-gradle-plugin-template"
+
+include(":example")
+includeBuild("plugin-build")
