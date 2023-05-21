75ecb24d7416f464182d854dd8d7bfb87954c078
undefined <iury.souza@klarna.com>
2023-05-21 13:30:06 +0200

    Apply custom themeVariables to sample project

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,7 +8,18 @@ plugins {
 moduleGraphConfig {
     heading.set("# Module Graph")
     readmePath.set("./README.md")
-    theme.set(Theme.DARK)
+    theme.set(
+        Theme.BASE(
+            mapOf(
+                "primaryTextColor" to "#fff", // All text colors
+                "primaryColor" to "#5a4f7c", // Box colors
+                "primaryBorderColor" to "#5a4f7c", // Box border color
+                "lineColor" to "#f5a623", // Line color
+                "tertiaryColor" to "#40375c", // Container box background
+                "fontSize" to "11px"
+            )
+        )
+    )
     orientation.set(Orientation.TOP_TO_BOTTOM)
 }
 
