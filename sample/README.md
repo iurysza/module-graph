ac6d9561a4f8352778462ae52bfe7cf8193ee352
Iury Souza <iurysza@gmail.com>
2024-03-31 15:29:14 +0200

    Update sample config

--- sample/README.md
+++ sample/README.md
@@ -4,25 +4,19 @@
 %%{
   init: {
     'theme': 'base',
-	'themeVariables': {"primaryTextColor":"#F6F8FAff","primaryColor":"#5a4f7c","primaryBorderColor":"#5a4f7c","tertiaryColor":"#40375c","lineColor":"#f5a623","fontSize":"12px","darkMode":"false"}
+	'themeVariables': {"primaryTextColor":"#F6F8FAff","primaryColor":"#5a4f7c","primaryBorderColor":"#5a4f7c","tertiaryColor":"#40375c","lineColor":"#f5a623","fontSize":"12px"}
   }
 }%%
 
 graph TB
   subgraph sample
     alpha
-    beta
-    zeta
   end
   subgraph container
     delta
-    gama
   end
-  zeta --> beta
-  alpha --> zeta
-  alpha --> beta
-  alpha --> gama
   alpha --> delta
-  gama --> zeta
 
+classDef focus fill:#F5A622,stroke:#fff,stroke-width:2px,color:#fff;
+class delta focus
 ```
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -11,19 +11,19 @@ moduleGraphConfig {
     readmePath.set("./README.md")
 
     // optional configs
-    pattern.set(""".*zeta.*""")
+//    focusedNodesPattern.set("""^(?=.*container)(?!.*gama).*""")
     showFullPath.set(false)
     orientation.set(Orientation.TOP_TO_BOTTOM)
     linkText.set(LinkText.NONE)
     theme.set(
         Theme.BASE(
             themeVariables = mapOf(
-                "primaryTextColor" to "#F6F8FAff", // All text colors
-                "primaryColor" to "#5a4f7c", // Node color
-                "primaryBorderColor" to "#5a4f7c", // Node border color
+                "primaryTextColor" to "#F6F8FAff", // Text
+                "primaryColor" to "#5a4f7c", // Node
+                "primaryBorderColor" to "#5a4f7c", // Node border
                 "tertiaryColor" to "#40375c", // Container box background
                 "lineColor" to "#f5a623",
-                "fontSize" to "12px",
+                "fontSize" to "12px"
             ),
             focusColor = "#F5A622"
         )
