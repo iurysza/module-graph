03fa426ae8328e349ee82f8c262b4f324713d5eb
Iury Souza <iurysza@gmail.com>
2024-04-29 09:14:32 +0200

    Add setup-java action

--- plugin-build/modulegraph/build.gradle.kts
+++ plugin-build/modulegraph/build.gradle.kts
@@ -23,13 +23,13 @@ tasks.test {
     useJUnitPlatform()
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
