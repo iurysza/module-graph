00e4d0f6323c4574220c9f1a537993a96d63ac4f
iury souza <iurysza@users.noreply.github.com>
2024-04-30 20:17:01 +0200

    Fix crash when project name has no colon (#43)

--- plugin-build/modulegraph/src/main/kotlin/dev/iurysouza/modulegraph/graph/Ext.kt
+++ plugin-build/modulegraph/src/main/kotlin/dev/iurysouza/modulegraph/graph/Ext.kt
@@ -1,12 +1,15 @@
 package dev.iurysouza.modulegraph.graph
 
-internal fun String.getProjectName(showFullPath: Boolean): String {
-    return if (showFullPath) {
+internal fun String.getProjectName(
+    showFullPath: Boolean,
+): String = runCatching {
+    if (showFullPath) {
         this
     } else {
         this.split(":").last { it.isNotBlank() }
     }
-}
+}.getOrElse { this }
+
 internal fun StringBuilder.lineBreak() {
     append("\n")
 }
